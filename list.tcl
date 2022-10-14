fe1494acc0721a105634cc2d3903b2aec2dbad31
sumaya-yesmin-primesilicon <sumaya.yesmin@primesilicon.com>
2022-10-14 12:05:38 +0600

    version_1

--- list.tcl
+++ list.tcl
@@ -1,37 +1,60 @@
-1 #lindex
-  2 set x "a b c d e"
-  3 puts [lindex $x]
-  4 
-  5 #finding position of veriable
-  6 puts [lindex $x 0]
-  7 
-  8 puts [lindex $x 1]
-  9 
- 10 puts [lindex $x 2]
- 11 
- 12 puts [lindex $x 3]
- 13 
- 14 puts [lindex $x 4]
- 15 
- 16 #adding variables by lappend
- 17 puts [lappend x i j k ]
- 18 
- 19 #number of variables in set
- 20 puts [llength $x]
- 21 
- 22 #search a veriable from set
- 23 puts [lsearch $x j]
- 24 
- 25 #inserting variables middle of a set
- 26 puts [linsert $x 5 f g h]
- 27 
- 28 puts [set y [linsert $x 5 f g h]]
- 29 
- 30 #replace variables
- 31 puts [lreplace $y 5 7 p q r]
- 32 
- 33 #to set variables
- 34 puts $y
- 35 
- 36 #replacing one variables by another variables
- 37 puts [lset y 0 p]
+#lindex
+set x "a b c d e"
+puts [lindex $x]
+
+#a b c d e
+#finding position of veriable
+puts [lindex $x 0]
+#a
+
+puts [lindex $x 1]
+#b
+
+puts [lindex $x 2]
+#c
+
+puts [lindex $x 3]
+#d
+
+puts [lindex $x 4]
+#e
+
+
+#adding variables by lappend
+puts [lappend x i j k ]
+
+#a b c d e i j k
+
+#number of variables in set
+puts [llength $x]
+#8
+
+
+#search a veriable from set
+puts [lsearch $x j]
+#6
+
+
+#inserting variables middle of a set
+puts [linsert $x 5 f g h]
+#a b c d e f g h i j k
+
+
+puts [set y [linsert $x 5 f g h]]
+#a b c d e f g h i j k
+
+
+#replace variables
+puts [lreplace $y 5 7 p q r]
+#a b c d e p q r i j k
+
+
+#to set variables
+puts $y
+#a b c d e f g h i j k
+
+
+#replacing one variables by another variables
+puts [lset y 0 p]
+#p b c d e f g h i j k
+
